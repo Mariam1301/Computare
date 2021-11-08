08e673918e4e8e24186bd87a564bd62a51a2c016
Luka Chapidze <lukachapidzee@gmail.com>
2021-11-08 21:55:25 +0400

    Fixed About Us and Prices Bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
-import AboutUs from './Pages/AboutUs/AboutUs';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import ExpertGuides from './components/ExpertGuides/ExpertGuides';
-import Prices from './components/Prices/Prices';
-import AllServices from './components/AllServices/AllServices';
-import SingleExpertGuide from './Pages/SingleExpertGuide/SingleExpertGuide';
-import Homepage from './Pages/homePage/HomePage';
-import './App.css';
+import { Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import AboutUs from './Pages/AboutUs/AboutUs'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import ExpertGuides from './components/ExpertGuides/ExpertGuides'
+import Prices from './components/Prices/Prices'
+import AllServices from './components/AllServices/AllServices'
+import SingleExpertGuide from './Pages/SingleExpertGuide/SingleExpertGuide'
+import Homepage from './Pages/homePage/HomePage'
+import PricesPage from './Pages/Prices/PricesPage'
+import './App.css'
 
 function App() {
 	return (
@@ -16,7 +17,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path='/guides/:id' element={<SingleExpertGuide />} />
-				<Route path='/prices' element={<Prices />} />
+				<Route path='/prices' element={<PricesPage />} />
 				<Route path='/services' element={<AllServices />} />
 				<Route path='/about-us' element={<AboutUs />} />
 				<Route path='/' element={<Homepage />} />
@@ -24,7 +25,7 @@ function App() {
 			</Routes>
 			<Footer />
 		</BrowserRouter>
-	);
+	)
 }
 
-export default App;
+export default App
--- src/Pages/SingleExpertGuide/SingleExpertGuide.js
+++ src/Pages/SingleExpertGuide/SingleExpertGuide.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import guides from '../../components/ExpertGuides/guides'
 import { useParams } from 'react-router'
+import { useEffect } from 'react'
 import './singleExpertGuide.css'
 
 const SingleExpertGuide = (props) => {
+	useEffect(() => {
+		window.scroll({ top: 0, behavior: 'smooth' })
+	})
 	const params = useParams()
 	const guide = guides.find((g) => g._id === params.id)
 	return (
