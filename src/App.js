30213b8f3e233a52078c6d129f6f9b74a9cbabc4
Mariam <marikhachvani13@gmail.com>
2021-11-08 16:08:45 +0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Routes } from 'react-router'
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 
 function App() {
 	return (
 		<div>
 			{/* <h1>I love you Marmaarr</h1> */}
-			<BrowserRouter>
-				<Header />
-				<Routes></Routes>
-				<Footer />
-			</BrowserRouter>
+			<h1>hello</h1>
+			<Header />
+			<Routes></Routes>
+			<Footer />
 			{/* <Footer /> */}
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
