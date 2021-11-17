171112423055c980887a74b67a9cd1a940abbd2b
Luka Chapidze <lukachapidzee@gmail.com>
2021-11-17 14:42:53 +0400

    Minor Bug Fixes

--- src/Pages/SingleExpertGuide/SingleExpertGuide.js
+++ src/Pages/SingleExpertGuide/SingleExpertGuide.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import guides from '../../components/ExpertGuides/guides';
-import { useParams } from 'react-router';
-import { useEffect } from 'react';
-import './singleExpertGuide.css';
+import React from 'react'
+import guides from '../../components/ExpertGuides/guides'
+import { useParams } from 'react-router'
+import { useEffect } from 'react'
+import './singleExpertGuide.css'
 
 const SingleExpertGuide = (props) => {
 	useEffect(() => {
-		window.scroll({ top: 0, behavior: 'smooth' });
-	}, []);
-	const params = useParams();
-	const guide = guides.find((g) => g._id === params.id);
+		window.scroll({ top: 0, behavior: 'smooth' })
+	}, [])
+	const params = useParams()
+	const guide = guides.find((g) => g._id === params.id)
 	return (
-		<div className='singleExpert-container'>
-			<div className='singleExpert-title'>
-				<h2>{guide.title}</h2>
+		<>
+			<div className='singleExpert-header'></div>
+			<div className='singleExpert-container'>
+				<div className='singleExpert-title'>
+					<h2>{guide.title}</h2>
+				</div>
+				<div className='singleExpert-info'>
+					{guide.info.map((i) => {
+						return <p>{i}</p>
+					})}
+				</div>
 			</div>
-			<div className='singleExpert-info'>
-				{guide.info.map((i) => {
-					return <p>{i}</p>;
-				})}
-			</div>
-		</div>
-	);
-};
+		</>
+	)
+}
 
-export default SingleExpertGuide;
+export default SingleExpertGuide
